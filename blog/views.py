4567adb80f130b2376cec8ec971df08cd0fb00da
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-03-19 14:53:58 +0000

    master

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    #id=request.session['id']
-    return render(request, 'homepage.html', context={})
+    if request.session['user_id']:
+        id=request.session['user_id']
+    else:
+        id=None
+    return render(request, 'homepage.html', context={'id':id})
 # Create your views here.
