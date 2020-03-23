73d2ededf7ffc028a92d62b6ebe1e1fada5a9996
DownD <miguelmartins17@hotmail.com>
2020-03-23 21:43:34 +0000

    Update Inscrição 2 tabs Backend

--- atividades/forms.py
+++ atividades/forms.py
@@ -12,4 +12,32 @@ class Form_Create_Atividades(ModelForm):
         model = models.Atividade
         fields = ['titulo','capacidade','duracao','descricao','espaco_idespaco']
     
+class Form_Responsaveis(ModelForm):
 
+    def fill(self, idinscricao):
+        self.cleaned_data['idinscricao'] = idinscricao
+        
+    class Meta:
+        model = models.Responsaveis
+        fields = ['nome','email']
+
+class Form_Inscricao(ModelForm):
+    class Meta:
+        model = models.Inscricao
+        fields = ['ano','local','areacientifica']
+
+class Form_InscricaoColetiva(ModelForm):
+
+    def fill(self, idUtilizador, idEscola, idInscricao):
+        self.cleaned_data['participante_utilizador_idutilizador'] = idUtilizador
+        self.cleaned_data['escola_idescola'] = idEscola
+        self.cleaned_data['inscricao_idinscricao '] = idInscricao
+
+    class Meta:
+        model = models.InscricaoColetiva
+        fields = ['turma']
+
+class Form_Escola(ModelForm):
+    class Meta:
+        model = models.Escola
+        fields = ['nome','local','telefone','email']
