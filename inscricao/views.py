804426e7297337bd43f5bedcd86cbdb15bda675c
Miguel <miguelmartins17@hotmail.com>
2020-04-26 17:48:01 +0100

    up

--- inscricao/views.py
+++ inscricao/views.py
@@ -44,24 +44,30 @@ def consultar_inscricao(request):
 
     query = models.InscricaoColetiva.objects.select_related('inscricao_idinscricao','escola_idescola').filter(participante_utilizador_idutilizador=user.pk)
     coletivas = query.values('turma',escola=F('escola_idescola__nome'),area=F('inscricao_idinscricao__areacientifica'),participantes=F('nparticipantes'))
+
+    result = []
     
-    for inscricao in query:
-        coletivas.almocos = models.InscricaoHasPrato.objects.select_related('prato_idprato','prato_idprato__menu_idmenu','prato_idprato__menu_idmenu__campus_idcampus').filter(inscricao_idinscricao=inscricao.inscricao_idinscricao)\
+    i=0
+    for inscricao in coletivas:
+        row = dict(inscricao)
+        row['almoco'] = models.InscricaoHasPrato.objects.select_related('prato_idprato','prato_idprato__menu_idmenu','prato_idprato__menu_idmenu__campus_idcampus').filter(inscricao_idinscricao=query[i].inscricao_idinscricao)\
             .values(campus=F('prato_idprato__menu_idmenu__campus_idcampus__nome'),menu=F('prato_idprato__menu_idmenu__menu'),tipo=F('prato_idprato__menu_idmenu__tipo'),descricao=F('prato_idprato__descricao'),nralmocos=F('prato_idprato__nralmocos'))\
             .order_by('prato_idprato__menu_idmenu__campus_idcampus__nome','prato_idprato__menu_idmenu__tipo')
         
-        coletivas.transportes = models.TransporteHasInscricao.objects.select_related('paragem','horario_has_dia').filter(inscricao_idinscricao=inscricao.inscricao_idinscricao)\
+        row['transportes'] = models.TransporteHasInscricao.objects.select_related('paragem','horario_has_dia').filter(inscricao_idinscricao=query[i].inscricao_idinscricao)\
             .values('partida_paragem','chegada_paragem',passageiros=F('numero_passageiros'),hora=F('partida'))
 
-        coletivas.responsaveis = models.Responsaveis.objects.filter(idinscricao=inscricao.inscricao_idinscricao)\
+        row['responsaveis'] = models.Responsaveis.objects.filter(idinscricao=query[i].inscricao_idinscricao)\
             .values('nome','telefone','email')
 
-        coletivas.sessoes = models.InscricaoHasSessao.objects \
+        row['sessoes'] = models.InscricaoHasSessao.objects \
             .select_related('sessao_idsessao__atividade_idatividade','sessao_idsessao__atividade_idatividade__departamento_iddepartamento','sessao_idsessao__atividade_idatividade__campus','sessao_idsessao__atividade_idatividade__unidade_organica_iduo__campus_idcampus','sessao_idsessao__atividade_idatividade__unidade_organica_iduo','sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora','sessao_idsessao__atividade_idatividade__professor_universitario_utilizador_idutilizador__utilizador_idutilizador','sessao_idsessao__atividade_idatividade__espaco_idespaco')\
-            .filter(inscricao_idinscricao=inscricao.inscricao_idinscricao)\
-            .order_by('atividade_idatividade__titulo','horario_has_dia_id_dia_hora__horario_hora__hora')\
+            .filter(inscricao_idinscricao=query[i].inscricao_idinscricao)\
+            .order_by('sessao_idsessao__atividade_idatividade__titulo','sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora__hora')\
             .values('nrinscritos',idsessao=F('sessao_idsessao__idsessao'),vagas=F('sessao_idsessao__vagas'),hora=F('sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora__hora'),idatividade=F('sessao_idsessao__atividade_idatividade__idatividade'),titulo=F('sessao_idsessao__atividade_idatividade__titulo'),duracao=F('sessao_idsessao__atividade_idatividade__duracao'),descricao=F('sessao_idsessao__atividade_idatividade__descricao'),unidade_organica=F('sessao_idsessao__atividade_idatividade__unidade_organica_iduo__sigla'),campus=F('sessao_idsessao__atividade_idatividade__unidade_organica_iduo__campus_idcampus__nome'),departamento=F('sessao_idsessao__atividade_idatividade__departamento_iddepartamento__nome'),tematica=F('sessao_idsessao__atividade_idatividade__tematica'),docente = F('sessao_idsessao__atividade_idatividade__professor_universitario_utilizador_idutilizador__utilizador_idutilizador__nome'),espaco = F('sessao_idsessao__atividade_idatividade__espaco_idespaco__nome'))
-        
+        i+=1
+        result.append(row)
 
-    return render(request,'consulta_inscricao.html',{'inscricoes':coletivas})
+
+    return render(request,'consulta_inscricao.html',{'inscricoes':result})
     
