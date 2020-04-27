0fd54af80163beecf0113312deae6d83e52cbe2c
guilhascorreia24 <55436745+guilhascorreia24@users.noreply.github.com>
2020-04-27 16:59:37 +0100

    master

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,6 @@ def home(request):
 # Create your views here.
 
 def encrypt(txt):
-    try:
         # convert integer etc to string first
         txt = str(txt)
         # get the key from settings
@@ -44,10 +43,6 @@ def encrypt(txt):
         # encode to urlsafe base64 format
         encrypted_text = base64.urlsafe_b64encode(encrypted_text).decode("ascii") 
         return encrypted_text
-    except Exception as e:
-        # log the error if any
-        logging.getLogger("error_logger").error(traceback.format_exc())
-        return None
 def decrypt(txt):
         # base64 decode
         txt = base64.urlsafe_b64decode(txt)
