70db3c4fbdd8fe68a69f81e7981a714406460203
Miguel <miguelmartins17@hotmail.com>
2020-05-18 00:00:53 +0100

    up

--- les/settings.py
+++ les/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'les',
         'USER': 'root',
-        'PASSWORD': '1999',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
