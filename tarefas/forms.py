30349eb507deae4bdd059967421694a6284725df
Rafael Figueira <rsimaofigueira@gmail.com>
2020-05-26 00:10:56 +0100

    Ez20

--- tarefas/forms.py
+++ tarefas/forms.py
@@ -3,9 +3,6 @@ from django import forms
 from tarefas import models
 
 class Form_Disponibilidade(ModelForm):
-    Dia=forms.DateField()
-    Horario=forms.TimeField()
-    Tipo_tarefa=forms.CharField()
 
     def save(self, colaborador_utilizador_idutilizador):
         base = super(Form_Disponibilidade, self).save(commit=False)
@@ -15,4 +12,4 @@ class Form_Disponibilidade(ModelForm):
 
     class Meta:
         model = models.Disponibilidade
-        fields = ['dia_dia','horario_hora','tipo_de_tarefa']
+        fields = ['dia_dia','horario_hora','horario_hora1','tipo_de_tarefa']
