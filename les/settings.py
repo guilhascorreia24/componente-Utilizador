7d92becaaafc9452d60b62ac3df685211610eaca
Rafael Figueira <rsimaofigueira@gmail.com>
2020-06-18 17:08:15 +0100

    oioi

--- les/settings.py
+++ les/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'les',
         'USER': 'root',
-        'PASSWORD': '12345abcd',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
