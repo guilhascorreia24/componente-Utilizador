c61209e1800c1039bdbe9cb3cfd152c3fd752d96
Miguel <miguelmartins17@hotmail.com>
2020-06-19 18:59:48 +0100

    update participante

--- inscricao/urls.py
+++ inscricao/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('register_individual',views.inscricao_individual_form,name='register_individual'),
     path('delete/<int:inscricao>',views.inscricao_delete,name='delete'),
     path('alterar/<int:inscricao>',views.inscricao_alterar,name='alterar'),
-    path('test',views.test,name='test')
 ]
