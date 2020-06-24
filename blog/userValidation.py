4a0ba007f7a42f1b82d7f76425e226d6daccdc3a
Miguel <miguelmartins17@hotmail.com>
2020-06-24 04:56:22 +0100

    Consultar e Filtros

--- blog/userValidation.py
+++ blog/userValidation.py
@@ -22,26 +22,31 @@ def getCurrentUser(_id):
 
     if user.validada == 0:
         part = Participante.objects.select_related('utilizador_idutilizador').get(utilizador_idutilizador = user)
+        part.user = user
         part._type = PARTICIPANTE
         return part
     
     if user.validada == 1:
         colab = Colaborador.objects.select_related('utilizador_idutilizador').get(utilizador_idutilizador = user)
+        colab.user = user
         colab._type = COLABORADOR
         return colab
     
     if user.validada == 2:
         coord = Coordenador.objects.select_related('utilizador_idutilizador').get(utilizador_idutilizador = user)
-        coord._type = COORDENADOR 
+        coord._type = COORDENADOR
+        coord.user = user
         return coord
 
     if user.validada == 3:
         prof._type=PROFESSOR_UNIVERSITARIO
         prof = ProfessorUniversitario.objects.select_related('utilizador_idutilizador').get(utilizador_idutilizador = user)
+        prof.user = user
         return prof
 
     if user.validada == 4:
         admin = Administrador.objects.select_related('utilizador_idutilizador').get(utilizador_idutilizador = user)
+        admin.user = user
         admin._type = ADMINISTRADOR 
         return admin
     
--- inscricao/views.py
+++ inscricao/views.py
@@ -110,17 +110,21 @@ def create_inscricao_allowed():
     
     return True
 
+def get_inscricoes(**kwargs):
+    if 'inscricao' in kwargs:
+        insc = kwargs['inscricao']
+    else:
+        insc = dict()
 
+    if 'sessao' in kwargs:
+        sessao_kwargs = kwargs['sessao']
+    else:
+        sessao_kwargs = dict()
 
-def consultar_inscricao(request):
-    user = userValidation.getLoggedUser(request)
-    if user._type != userValidation.PARTICIPANTE:
-        context={'i':len(noti_not_checked(request)),'not_checked':noti_not_checked(request)}
-        return render(request,"not_for-u.html",{'context' : context , 'message' : "Utilizador não é participante"})
-
-    query_coletiva = models.InscricaoColetiva.objects.select_related('inscricao_idinscricao','escola_idescola').filter(participante_utilizador_idutilizador=user.pk)
+    query_coletiva = models.InscricaoColetiva.objects.select_related('inscricao_idinscricao','escola_idescola','participante_utilizador_idutilizador__utilizador_idutilizador').filter(**insc)
     coletivas = query_coletiva.values('turma','nparticipantes',local=F('inscricao_idinscricao__local'),idinscricao=F('inscricao_idinscricao__idinscricao'),ano=F('inscricao_idinscricao__ano'),email=F('escola_idescola__email'),telefone=F('escola_idescola__telefone'),escola=F('escola_idescola__nome'),areacientifica=F('inscricao_idinscricao__areacientifica'))
-    query_individual = models.InscricaoIndividual.objects.select_related('inscricao_idinscricao').filter(participante_utilizador_idutilizador=user.pk)
+    
+    query_individual = models.InscricaoIndividual.objects.select_related('inscricao_idinscricao','participante_utilizador_idutilizador__utilizador_idutilizador').filter(**insc)
     individuais = query_individual.values('nracompanhantes','telefone',local=F('inscricao_idinscricao__local'),idinscricao=F('inscricao_idinscricao__idinscricao'),ano=F('inscricao_idinscricao__ano'),areacientifica=F('inscricao_idinscricao__areacientifica'))
     result_coletivo = []
     result_individual = []
@@ -130,6 +134,9 @@ def consultar_inscricao(request):
         row = dict()
         row['is_coletiva'] = 1
         row['inscricao'] = coletivas[i]
+
+        row['user'] =  models.Utilizador.objects.filter(idutilizador=query_coletiva[i].participante_utilizador_idutilizador.utilizador_idutilizador.pk)\
+            .values('email','nome','telefone')[0]
 
         row['almoco'] = models.InscricaoHasPrato.objects.select_related('prato_idprato','prato_idprato__menu_idmenu','prato_idprato__menu_idmenu__campus_idcampus').filter(inscricao_idinscricao=query_coletiva[i].inscricao_idinscricao)\
             .values('nralmocos',campus=F('prato_idprato__menu_idmenu__campus_idcampus__nome'),menu=F('prato_idprato__menu_idmenu__menu'),tipo=F('prato_idprato__tipo'),descricao=F('prato_idprato__descricao'))\
@@ -141,12 +148,18 @@ def consultar_inscricao(request):
         row['responsaveis'] = models.Responsaveis.objects.filter(idinscricao=query_coletiva[i].inscricao_idinscricao)\
             .values('nome','telefone','email')
 
+        sessao_kwargs['inscricao_idinscricao'] = query_coletiva[i].inscricao_idinscricao
+
         row['atividade'] = models.InscricaoHasSessao.objects \
             .select_related('sessao_idsessao__atividade_idatividade','sessao_idsessao__atividade_idatividade__departamento_iddepartamento','sessao_idsessao__atividade_idatividade__campus','sessao_idsessao__atividade_idatividade__unidade_organica_iduo__campus_idcampus','sessao_idsessao__atividade_idatividade__unidade_organica_iduo','sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora','sessao_idsessao__atividade_idatividade__professor_universitario_utilizador_idutilizador__utilizador_idutilizador','sessao_idsessao__atividade_idatividade__espaco_idespaco')\
-            .filter(inscricao_idinscricao=query_coletiva[i].inscricao_idinscricao)\
+            .filter(**sessao_kwargs)\
             .order_by('sessao_idsessao__atividade_idatividade__titulo','sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora__hora')\
             .values('nr_inscritos',idsessao=F('sessao_idsessao__idsessao'),vagas=F('sessao_idsessao__capacidade'),hora=F('sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora__hora'),idatividade=F('sessao_idsessao__atividade_idatividade__idatividade'),titulo=F('sessao_idsessao__atividade_idatividade__titulo'),duracao=F('sessao_idsessao__atividade_idatividade__duracao'),descricao=F('sessao_idsessao__atividade_idatividade__descricao'),unidade_organica=F('sessao_idsessao__atividade_idatividade__unidade_organica_iduo__sigla'),campus=F('sessao_idsessao__atividade_idatividade__unidade_organica_iduo__campus_idcampus__nome'),departamento=F('sessao_idsessao__atividade_idatividade__departamento_iddepartamento__nome'),tematica=F('sessao_idsessao__atividade_idatividade__tematica'),docente = F('sessao_idsessao__atividade_idatividade__professor_universitario_utilizador_idutilizador__utilizador_idutilizador__nome'),espaco = F('sessao_idsessao__atividade_idatividade__espaco_idespaco__nome'))
         i+=1
+
+        if(len(row['atividade']) == 0):
+            continue
+
         result_coletivo.append(row)
     
     j = 0
@@ -155,6 +168,9 @@ def consultar_inscricao(request):
         row['is_coletiva'] = 0
         row['inscricao'] = individuais[j]
 
+        row['user'] =  models.Utilizador.objects.filter(idutilizador=query_individual[j].participante_utilizador_idutilizador.utilizador_idutilizador.pk)\
+                       .values('email','nome','telefone')[0]
+
         row['almoco'] = models.InscricaoHasPrato.objects.select_related('prato_idprato','prato_idprato__menu_idmenu','prato_idprato__menu_idmenu__campus_idcampus').filter(inscricao_idinscricao=query_individual[j].inscricao_idinscricao)\
             .values('nralmocos',campus=F('prato_idprato__menu_idmenu__campus_idcampus__nome'),menu=F('prato_idprato__menu_idmenu__menu'),tipo=F('prato_idprato__tipo'),descricao=F('prato_idprato__descricao'))\
             .order_by('prato_idprato__menu_idmenu__campus_idcampus__nome','prato_idprato__tipo')
@@ -162,16 +178,40 @@ def consultar_inscricao(request):
         row['transportes'] = models.TransporteHasInscricao.objects.select_related('paragem','horario').filter(inscricao_idinscricao=query_individual[j].inscricao_idinscricao)\
             .values(partida_paragem=F('horario__origem'),chegada_paragem=F('horario__destino'),passageiros=F('n_passageiros'),hora=F('horario__horario_has_dia_id_dia_hora'))
 
-        #row['responsaveis'] = models.Responsaveis.objects.filter(idinscricao=query[i].inscricao_idinscricao)\
-            #.values('nome','telefone','email')
+        sessao_kwargs['inscricao_idinscricao'] = query_individual[j].inscricao_idinscricao
 
         row['atividade'] = models.InscricaoHasSessao.objects \
             .select_related('sessao_idsessao__atividade_idatividade','sessao_idsessao__atividade_idatividade__departamento_iddepartamento','sessao_idsessao__atividade_idatividade__campus','sessao_idsessao__atividade_idatividade__unidade_organica_iduo__campus_idcampus','sessao_idsessao__atividade_idatividade__unidade_organica_iduo','sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora','sessao_idsessao__atividade_idatividade__professor_universitario_utilizador_idutilizador__utilizador_idutilizador','sessao_idsessao__atividade_idatividade__espaco_idespaco')\
-            .filter(inscricao_idinscricao=query_individual[j].inscricao_idinscricao)\
+            .filter(**sessao_kwargs)\
             .order_by('sessao_idsessao__atividade_idatividade__titulo','sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora__hora')\
             .values('nr_inscritos',idsessao=F('sessao_idsessao__idsessao'),vagas=F('sessao_idsessao__capacidade'),hora=F('sessao_idsessao__horario_has_dia_id_dia_hora__horario_hora__hora'),idatividade=F('sessao_idsessao__atividade_idatividade__idatividade'),titulo=F('sessao_idsessao__atividade_idatividade__titulo'),duracao=F('sessao_idsessao__atividade_idatividade__duracao'),descricao=F('sessao_idsessao__atividade_idatividade__descricao'),unidade_organica=F('sessao_idsessao__atividade_idatividade__unidade_organica_iduo__sigla'),campus=F('sessao_idsessao__atividade_idatividade__unidade_organica_iduo__campus_idcampus__nome'),departamento=F('sessao_idsessao__atividade_idatividade__departamento_iddepartamento__nome'),tematica=F('sessao_idsessao__atividade_idatividade__tematica'),docente = F('sessao_idsessao__atividade_idatividade__professor_universitario_utilizador_idutilizador__utilizador_idutilizador__nome'),espaco = F('sessao_idsessao__atividade_idatividade__espaco_idespaco__nome'))
         j+=1
+        if(len(row['atividade']) == 0):
+            continue
+
         result_individual.append(row)
+    
+    return (result_coletivo,result_individual)
+
+
+def consultar_inscricao(request):
+    user = userValidation.getLoggedUser(request)
+    if user._type == None:
+        context={'i':len(noti_not_checked(request)),'not_checked':noti_not_checked(request)}
+        return render(request,"not_for-u.html",{'context' : context , 'message' : "Não está logado"})
+    
+    if user._type == userValidation.ADMINISTRADOR:
+        args = {}
+
+    if user._type == userValidation.COORDENADOR:
+        sessao = {'sessao_idsessao__atividade_idatividade__unidade_organica_iduo':user.unidade_organica_iduo}
+        args = {'sessao':sessao}
+    
+    if user._type == userValidation.PARTICIPANTE:
+        insc={'participante_utilizador_idutilizador':user.pk}
+        args = {'inscricao':insc}
+
+    (result_coletivo,result_individual) = get_inscricoes(**args)
 
 
     return render(request,'consultar_participante.html',{'inscricoes_coletivas':result_coletivo,'inscricoes_individuais':result_individual,  'i':len(noti_not_checked(request)),'not_checked':noti_not_checked(request)})
