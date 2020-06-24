a7a6a403ec33e28d968151411f6307d58413c107
BrunooMPS <brunosusana99@hotmail.com>
2020-06-24 23:03:46 +0100

    h

--- menu/forms.py
+++ menu/forms.py
@@ -1,6 +1,6 @@
 from django.forms import *
 from django import forms
-from .models import *
+from models import *
 
 ##### Dia Aberto #####
 
--- tarefas/tests/test_urls.py
+++ tarefas/tests/test_urls.py
@@ -1,3 +1,17 @@
 from django.test import TestCase, SimpleTestCase
-from user.views import *
+from tarefas.views import *
 from django.urls import reverse,resolve
+
+class TestUrls(SimpleTestCase):
+
+    def test_disponibilidade(self):
+        url = reverse('tarefas:disponibilidade')
+        self.assertEquals(resolve(url).func, consultar_tarefas)
+    
+    def test_tarefas(self):
+        url = reverse('tarefas:tarefas')
+        self.assertEquals(resolve(url).func, consultar_tarefas2)
+    
+    def test_estado(self):
+        url = reverse('tarefas:mudar_estado')
+        self.assertEquals(resolve(url).func, mudar_estado)
