041cf7a48012eb79acfcc6ce012ab431dce07673
BrunooMPS <brunosusana99@hotmail.com>
2020-07-01 22:28:52 +0100

    h

--- les/settings.py
+++ les/settings.py
@@ -98,6 +98,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'les',
+        'USER':'root',
         'PASSWORD': '12345abcd',
         'HOST': '127.0.0.1',
         'PORT': '3306',
