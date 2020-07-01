1f37111fc18e2e6b856643e6bad037e90cda4f69
VascoBrito21 <61838749+VascoBrito21@users.noreply.github.com>
2020-07-01 16:40:34 +0100

    t

--- les/settings.py
+++ les/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'les',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': '81234',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
