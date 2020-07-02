0597f9dace3226bf043dacc07695ee708090dea4
RafikiGod <rsimaofigueira@gmail.com>
2020-07-02 20:49:06 +0100

    all done tarefas

--- les/settings.py
+++ les/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'les',
         'USER': 'root',
-        'PASSWORD': '1234',
+        'PASSWORD': '12345',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
